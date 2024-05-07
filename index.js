3f0dc3c99f809c19d331dc2a14116a4756151976
Ariadna <139250178+AriadnaLZ@users.noreply.github.com>
2024-05-07 12:32:45 +0200

    Add files via upload

--- index.js
+++ index.js
@@ -3,20 +3,36 @@ const fs = require('fs')
 
 const charactersArray = []
 
+
 const scrapper = async (url) => {
 console.log(url)
+let count = 0
+let fallo = 0
 
 const browser = await puppeteer.launch({headless: false})
 const page = await browser.newPage()
+
 await page.goto(url)
 await page.setViewport({width: 1080, height: 1024})
 
 const arrayDivs = await page.$$('.simple-card')
+
+
+await page.evaluate(() => {
+  window.scrollTo(0, document.body.scrollHeight);
+})
+
+
+
+
 for (const characterDiv of arrayDivs) {
   let description
   let img 
   let title
+  count += 1
+  // console.log(count)
 
+  await page.waitForSelector('.flip0')
    try {
     title =   await characterDiv.$eval('.cardname', (el) => el.textContent)
    } catch (error) {
@@ -29,10 +45,17 @@ for (const characterDiv of arrayDivs) {
   }
 
   try {
-    img = await characterDiv.$eval('img', (el) => el.src)
+    img = await characterDiv.$eval('.flip0', (el) => el.src)
+    if (img === "https://marvelsnapzone.com/wp-content/themes/blocksy-child/assets/media/blank.card.png?v1") {
+      fallo += 1
+      console.log(`Han fallado ${fallo}`)
+      console.log(title)
+    }
+  
   } catch (error) {
     img = 'Image not found'
   }
+
 
 
   const character = {
@@ -47,6 +70,8 @@ for (const characterDiv of arrayDivs) {
 
 
 write(charactersArray)
+
+await browser.close()
 }
 
 
